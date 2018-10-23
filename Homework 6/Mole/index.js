e16dc9fd18634cfa397dcaba5bc9abbe091a7bb8
Jiahonzheng <jiahonzheng@gmail.com>
2018-10-23 20:04:28 +0800

    HW6 Finish Mole

--- Homework 6/Mole/index.js
+++ Homework 6/Mole/index.js
@@ -5,24 +5,27 @@ window.onload = function() {
   const result = document.getElementById("result");
   const score = document.getElementById("score");
 
+  const MSG_READY = "Ready to Play";
   const MSG_PLAYING = "Playing";
   const MSG_LOSE = "Game Over";
 
   let isStarting = false;
   let count = 0;
   let timer = null;
-  let remainTime;
+  let remainTime = 30;
+  let mole = null;
 
   setHole();
+
+  const holes = document.getElementsByClassName("hole");
 
   controller.addEventListener("click", onControllerPressed);
 
   function setHole() {
     for (let i = 0; i < 60; i++) {
-      const element = document.createElement("input");
+      const element = document.createElement("div");
 
-      element.setAttribute("type", "radio");
-      element.setAttribute("name", "hole");
+      element.setAttribute("data-id", i);
       element.className = "hole";
       element.addEventListener("click", onHoleWhacked);
 
@@ -32,7 +35,7 @@ window.onload = function() {
 
   function setTime() {
     if (!timer) {
-      remainTime = 3;
+      remainTime = 30;
     } else {
       remainTime--;
     }
@@ -40,53 +43,64 @@ window.onload = function() {
     time.value = remainTime;
 
     if (remainTime === 0) {
-      resetTime();
+      isStarting = false;
+      mole.className = "hole";
+      clearInterval(timer);
+      timer = null;
       setResult(MSG_LOSE);
+      count = 0;
     }
-  }
-
-  function resetTime() {
-    isStarting = false;
-    time.value = 3;
-    clearInterval(timer);
-    timer = null;
   }
 
   function setResult(data) {
     result.value = data;
   }
 
-  function resetResult() {
-    result.value = "";
-  }
-
   function setScore(data) {
     score.value = data;
-  }
-
-  function resetScore() {
-    score.value = 0;
   }
 
   function onControllerPressed() {
     if (isStarting) {
       isStarting = false;
-      resetTime();
-      resetResult();
-      resetScore();
+      mole.className = "hole";
+      clearInterval(timer);
+      timer = null;
+      setResult(MSG_READY);
+      count = 0;
       return;
     }
 
     isStarting = true;
-
+    mole = holes[Math.ceil((holes.length - 1) * Math.random())];
+    mole.className = "mole";
+    setResult(MSG_PLAYING);
+    setScore(0);
     setTime();
     timer = setInterval(setTime, 1000);
   }
 
-  function onHoleWhacked() {
+  function onHoleWhacked(event) {
     if (!isStarting) {
       return;
     }
+
+    const { id } = event.target.dataset;
+
+    if (id !== mole.dataset.id) {
+      setScore(--count);
+      return;
+    }
+
+    let index;
+
+    do {
+      index = Math.ceil((holes.length - 1) * Math.random());
+    } while (index === parseInt(mole.dataset.id));
+
+    mole.className = "hole";
+    mole = holes[index];
+    mole.className = "mole";
 
     setScore(++count);
   }
