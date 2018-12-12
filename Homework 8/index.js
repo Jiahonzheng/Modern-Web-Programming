8b5d53c6c4d232d0c1825a8bd97646fe0c1e8a8b
Mr. Tseng <jiahonzheng@gmail.com>
2018-12-12 22:12:46 +0800

    HW8 Add index.html

--- Homework 8/index.js
+++ Homework 8/index.js
@@ -3,6 +3,71 @@ const url = require("url");
 const path = require("path");
 const fs = require("fs");
 
+const html = `
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <title>环形按钮</title>
+    <meta charset="utf-8" />
+    <link rel="icon" sizes="196x196" href="/S1/assets/images/favicon.png" />
+    <style>
+      body {
+        background: #2d343d;
+      }
+
+      .main {
+        bottom: 0;
+        height: 350px;
+        left: 0;
+        margin: auto;
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 300px;
+      }
+
+      div {
+        display: block;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        text-align: center;
+      }
+
+      button {
+        background: #f0776c;
+        border: none;
+        cursor: pointer;
+        height: 50px;
+        width: 300px;
+        font-family: "Arial";
+        font-size: 22px;
+        color: #ffffff;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class="main">
+      <div>
+        <a href="S1/index.html"><button>S1 - 人工交互</button></a>
+      </div>
+      <div>
+        <a href="S2/index.html"><button>S2 - 仿真机器人，顺序</button></a>
+      </div>
+      <div>
+        <a href="S3/index.html"><button>S3 - 仿真机器人，并行</button></a>
+      </div>
+      <div>
+        <a href="S4/index.html"><button>S4 - 仿真机器人，随机</button></a>
+      </div>
+      <div>
+        <a href="S5/index.html"><button>S5 - 仿真机器人，独立行为</button></a>
+      </div>
+    </div>
+  </body>
+</html>
+`;
+
 const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
 const mime = {
   ".html": "text/html",
@@ -20,7 +85,7 @@ const server = http.createServer(function(req, res) {
   req.pathname = pathname;
   req.mime = mime[path.extname(pathname)];
 
-  if (req.mime === undefined) {
+  if (pathname === "/api" && req.mime === undefined) {
     cors(req, res, handleAjax);
     return;
   }
@@ -61,8 +126,10 @@ function handleStatic(req, res) {
 
   fs.readFile(filePath, function(err, data) {
     if (err) {
-      res.writeHead(500);
-      res.end();
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(html);
       return;
     }
 
